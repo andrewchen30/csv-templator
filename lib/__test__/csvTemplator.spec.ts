bafee0b2632247051b0d71a97b8e7334146a6f55
AndrewChen <chenpoanandrew@gmail.com>
2023-01-05 00:34:52 +0800

    feat: render raw output

--- lib/__test__/csvTemplator.spec.ts
+++ lib/__test__/csvTemplator.spec.ts
@@ -96,4 +96,35 @@ describe('CSV Templator', () => {
       expect(templator._rawValue).toMatchSnapshot('_rawValue');
     });
   });
+
+  describe('render output', () => {
+    const successCases: {
+      desc: string;
+      template: string;
+      data: any;
+    }[] = [
+      {
+        desc: 'should successfully prepare data for render',
+        template: `
+        | | "name" | "age"
+        | // for-row user in users | user.name | user.age |
+      `,
+        data: {
+          users: [
+            { name: 'Andrew', age: 30 },
+            { name: 'Joanne', age: 28 },
+            { name: 'Frank', age: 22 },
+          ],
+        },
+      },
+    ];
+
+    it.each(successCases)('$desc', ({ template, data }) => {
+      const templator = new CSVTemplator();
+      templator.useTemplate(template);
+      templator.render(data);
+
+      expect(templator._rawOutput).toMatchSnapshot('_rawValue');
+    });
+  });
 });
